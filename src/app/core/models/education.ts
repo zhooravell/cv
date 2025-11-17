5eb85ca4a543d0211d5b00b49d1481e2ba106a08
Roman Zhuravel <roman.zhuravel@monobank.ua>
2025-11-17 12:22:43 +0200

    educations

--- src/app/core/models/education.ts
+++ src/app/core/models/education.ts
@@ -1,4 +1,5 @@
 export interface Education {
+  id: number;
   school: string;
   city: string;
   schoolLogo: string;
--- src/app/features/cv/components/education/education.component.ts
+++ src/app/features/cv/components/education/education.component.ts
@@ -1,15 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '../../../../core/services/translate/translate.service';
+import {LanguageService} from '../../../../core/services/language/language.service';
+import {Education} from '../../../../core/models/education';
+import {EducationService} from '../../../../core/services/education/education.service';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-education',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './education.component.html',
   styleUrl: './education.component.scss',
 })
-export class EducationComponent {
+export class EducationComponent implements OnInit {
+  protected education: Education[] = [];
+  protected lang: string = '';
+
   constructor(
+    private educationService: EducationService,
+    private languageService: LanguageService,
     protected translateService: TranslateService,
   ) {
+  }
+
+  ngOnInit(): void {
+    this.lang = this.languageService.currentLocale;
+    this.load();
+    this.languageService.locale$.subscribe(lang => {
+      this.lang = lang;
+      this.load()
+    });
+  }
+
+  private load() {
+    this.educationService.get(this.lang).subscribe({
+      next: (res) => {
+        this.education = res;
+      },
+      error: (error) => console.error(error),
+    });
   }
 }
